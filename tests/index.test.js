a69415bdd1cc3225f24ff5192a7e8885218c48f3
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-03-26 02:16:43 +0100

    feat: Prefer Webpack + Babel Loader build

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import emarsysRegistrationForm from '../src';
+import emarsysRegistrationForm from '../src/setup';
 
 test('returns failure E001 when no parameters are passed', () => {
   expect(emarsysRegistrationForm.setup().success).toBe(false);
